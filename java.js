a94f1eebddca1f2135a4db9fe840ad77573aac6d
Hunain <hunainaslam.ha@gmail.com>
2022-12-16 00:35:27 +0500

    Initial Commit

---
+++ java.js
@@ -0,0 +1,8 @@
+function cal()
+{
+    var a = parseInt(document.getElementById("txtin").value);
+    var b = parseInt(document.getElementById("txtin").value);
+
+{
+    window.location.replace("reservation2.html");
+}}
